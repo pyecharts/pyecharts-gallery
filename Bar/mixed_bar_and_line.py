663817abd0f4c31328187f8acb8bb7ea73aa534e
sunhailinLeo <379978424@qq.com>
2019-05-01 00:14:57 +0800

    :hammer: 更新文档和部分代码

--- Bar/mixed_bar_and_line.py
+++ Bar/mixed_bar_and_line.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 import pyecharts.options as opts
-from pyecharts.charts import Bar
-from pyecharts.globals import ChartType
+from pyecharts.charts import Bar, Line
 
 """
 Gallery 使用 Pyecharts 1.0.0
@@ -10,12 +9,12 @@ Gallery 使用 Pyecharts 1.0.0
 
 目前无法实现的功能:
 
-1、tooltip 失效
+1、暂无
 """
 
 x_data = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
 
-c = (
+bar = (
     Bar(init_opts=opts.InitOpts(width="1600px", height="800px"))
     .add_xaxis(xaxis_data=x_data)
     .add_yaxis(
@@ -54,13 +53,6 @@ c = (
         ],
         label_opts=opts.LabelOpts(is_show=False),
     )
-    .add_yaxis(
-        series_name="平均温度",
-        type_=ChartType.LINE,
-        yaxis_index=1,
-        yaxis_data=[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
-        label_opts=opts.LabelOpts(is_show=False),
-    )
     .extend_axis(
         yaxis=opts.AxisOpts(
             name="温度",
@@ -72,9 +64,9 @@ c = (
         )
     )
     .set_global_opts(
-        tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+        tooltip_opts=opts.TooltipOpts(is_show=True, trigger="axis", axis_pointer_type="cross"),
         xaxis_opts=opts.AxisOpts(
-            type_="category", axispointer_opts=opts.AxisPointerOpts(type_="shadow")
+            type_="category", axispointer_opts=opts.AxisPointerOpts(is_show=True, type_="shadow")
         ),
         yaxis_opts=opts.AxisOpts(
             name="水量",
@@ -87,5 +79,18 @@ c = (
             splitline_opts=opts.SplitLineOpts(is_show=True),
         ),
     )
-    .render("mixed_bar_and_line.html")
 )
+
+line = (
+    Line()
+    .add_xaxis(xaxis_data=x_data)
+    .add_yaxis(
+        series_name="平均温度",
+        yaxis_index=1,
+        y_axis=[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+)
+
+#
+bar.overlap(line).render("mixed_bar_and_line.html")
